064fd38b251fe7ed5b3be1dbdb830a5d842d8720
Antonios Gkamitsios <angam97@gmail.com>
2025-11-11 23:50:19 +0200

    Add simple cross origin protection middleware

---
+++ crossOriginProtection.js
@@ -0,0 +1,15 @@
+import { SAFE_METHODS } from './legit-site/utils.js';
+
+export function crossOriginProtection(req, res, next) {
+  if (SAFE_METHODS.includes(req.method)) {
+    next();
+    return;
+  }
+
+  if (req.headers['sec-fetch-site'] !== 'same-origin') {
+    res.status(400).send('blocked');
+    return;
+  }
+
+  next();
+}
